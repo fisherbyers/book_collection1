41c893c470f008006fec3919acfdcd15622b84a6
fisherbyers <fisherbyers@tamu.edu>
2023-09-12 21:34:54 -0500

    deleting shit

--- db/migrate/20230905185059_create_books.rb
+++ db/migrate/20230905185059_create_books.rb
@@ -2,9 +2,7 @@ class CreateBooks < ActiveRecord::Migration[7.0]
   def change
     create_table :books do |t|
       t.string :title
-      t.string :author
-      t.string :price
-      t.string :publishDate
+    
 
       t.timestamps
     end
