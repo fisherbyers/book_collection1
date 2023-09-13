124b04a8479a8af80e8b20f2dfa843de4ec7ff77
fisherbyers <fisherbyers@tamu.edu>
2023-09-12 21:09:06 -0500

    changing migrate

--- db/migrate/20230906022724_new_mig.rb
+++ db/migrate/20230906022724_new_mig.rb
@@ -1,7 +1,7 @@
 class NewMig < ActiveRecord::Migration[7.0]
   def change
-    unless column_exists?(:books, :author)
-      add_column :books, :author, :string
+
+    add_column :books, :author, :string
     add_column :books, :price, :string
     add_column :books, :publishDate, :string
   end
